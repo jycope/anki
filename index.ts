6a469b7e8ea92f2318bdd8f668a00be0112db3f0
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2023-07-07 22:56:31 +0500

    WIP: 12ccc4e init

--- index.ts
+++ index.ts
@@ -10,10 +10,69 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+
+interface Translator {
+  translate(text: Array<any>): any
+}
+abstract class Creator {
+  public abstract factoryMethod(): Translator
+  
+  public translate(): any
+  {
+    this.factoryMethod();
+  }
+}
+
+class YandexCreator extends Creator {
+  public factoryMethod(): Translator {
+    return new YandexTranslator();
+  }
+}
+
+class YandexTranslator implements Translator {
+  private apiKey = 't1.9euelZqXyMubkomJksydj5KJncjGx-3rnpWalpqanMqXk8uNx5yPz8fKlp3l8_cNQmBa-e9wKGx5_t3z901wXVr573AobHn-zef1656VmpaKjZaanJaVlJqeiprNz82U7_zF656VmpaKjZaanJaVlJqeiprNz82U.eVUBlBHq5hMpBEHU4AVzayF0EBJX77xFbnMbxGSLyVCOz3vx3fHpqzMW5PuBZqQ4rzYYJtnpg-Pr0gHZjEK4DA';
+
+  private headers = {
+    'Content-Type': 'text/plain',
+    'Authorization': `Bearer ${this.apiKey}`
+  }
+
+  private folderId = 'b1g5moh8p0918sljpfjr'
+  private url = `https://translate.api.cloud.yandex.net/translate/v2/translate?folderId=${this.folderId}`
+  
+  public async translate( text ): Promise<any>
+  {
+    const circularReplacer = () => {
+      const seen = new WeakSet();
+      return (key, value) => {
+        if (typeof value === 'object' && value !== null) {
+          if (seen.has(value)) {
+            return '[Circular]';
+          }
+          seen.add(value);
+        }
+        return value;
+      };
+    };
+
+
+    let options = {
+      sourceLanguageCode: 'en',
+      targetLanguageCode: 'ru',
+      texts: text
+    };
+
+    console.log(3);
+    
+    
+    const headers = this.headers
+    
+    return await axios.post(this.url, options, { headers })
+  }
+}
+
 app.post('/word', async (req: any, res: any) => {
   const url = `https://dictionary.cambridge.org/dictionary/english-russian/${req.body.word}`;
-  const apiKey = 't1.9euelZqXyMubkomJksydj5KJncjGx-3rnpWalpqanMqXk8uNx5yPz8fKlp3l8_cNQmBa-e9wKGx5_t3z901wXVr573AobHn-zef1656VmpaKjZaanJaVlJqeiprNz82U7_zF656VmpaKjZaanJaVlJqeiprNz82U.eVUBlBHq5hMpBEHU4AVzayF0EBJX77xFbnMbxGSLyVCOz3vx3fHpqzMW5PuBZqQ4rzYYJtnpg-Pr0gHZjEK4DA';
-  const folderId = 'b1g5moh8p0918sljpfjr'
   
   async function getExamples() {
     try {
@@ -32,32 +91,9 @@ app.post('/word', async (req: any, res: any) => {
     } catch (error) {
       console.log('Ошибка при выполнении запроса:', error);
     }
-  } 
+  }
 
-  const examples = await getExamples();
-
-  res.send(examples);
-
-  const data = JSON.stringify({
-    sourceLanguageCode: 'en',
-    targetLanguageCode: 'ru',
-    texts: examples
-  });
-
-
-  const urlYandex = `https://translate.api.cloud.yandex.net/translate/v2/translate?folderId=${folderId}`;
-  const headers = {
-    'Content-Type': 'text/plain',
-    'Authorization': `Bearer ${apiKey}`
-  };
-
-  axios.post(urlYandex, data, { headers })
-    .then(response => {
-      res.send(response.data);
-    })
-    .catch(error => {
-      res.send(error);
-    });
+  res.send(await new YandexTranslator().translate(await getExamples()))
 
 });
 
